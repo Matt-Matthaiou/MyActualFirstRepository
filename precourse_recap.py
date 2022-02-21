2b0fec86566cd714c660fba8728e01b000f91d3e
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-02-21 19:36:25 +0000

    First commmit

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+enemyName = "Orc Warrior"
+hp = 30
+attackDamage = 10.5
+ranged = False
+
+print("An", enemyName, "appeared!")
+
+remainingHP = hp - attackDamage
+
+print("The", enemyName, "deals", attackDamage, "points of damage to you with his hammer.")
+print("your remaining health is now:", remainingHP)
