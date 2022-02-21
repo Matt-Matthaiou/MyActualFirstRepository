7284438d1e46cd1325d19b116e8b46b7369c0c63
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-02-21 19:43:15 +0000

    added capitalize() to my string

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,11 +1,11 @@
-enemyName = "Orc Warrior"
+enemyName = "orc warrior"
 hp = 30
 attackDamage = 10.5
 ranged = False
 
-print("An", enemyName, "appeared!")
+print("An", enemyName.capitalize(), "appeared!")
 
 remainingHP = hp - attackDamage
 
-print("The", enemyName, "deals", attackDamage, "points of damage to you with his hammer.")
+print("The", enemyName.capitalize(), "deals", attackDamage, "points of damage to you with his hammer.")
 print("your remaining health is now:", remainingHP)
